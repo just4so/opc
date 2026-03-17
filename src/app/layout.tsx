ed9f996a27ab224594170353bf660f84ad41d982
just4so <61100979+just4so@users.noreply.github.com>
2026-03-17 12:03:14 +0800

    feat: 添加百度自动推送JS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,6 +59,22 @@ export default function RootLayout({
             }),
           }}
         />
+        {/* 百度自动推送 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    } else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();`,
+          }}
+        />
       </head>
       <body className={inter.className}>
         <SessionProvider>{children}</SessionProvider>
